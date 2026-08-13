981324bf522e778ae2d723bb63a2acf3f46ea3de
Xingfan Xia <xingfanxia@gmail.com>
2026-08-13 12:08:32 -0700

    docs: add mobile privacy policy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: './',
+  build: {
+    rollupOptions: {
+      input: ['index.html', 'privacy.html'],
+    },
+  },
   plugins: [react()],
 })
